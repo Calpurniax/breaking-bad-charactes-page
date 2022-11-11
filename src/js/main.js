292b3db76d16f431b1061c661b2d2ea59c17d16e
Calpurniax <sofia.gracia@gmail.com>
2022-11-11 18:05:18 +0100

    add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 'use strict'
+//query selector
+const btnSearch = document.querySelector('.js-btn');
+const inputSearch = document.querySelector('.js-text');
+const list = document.querySelector('.js-list');
+const paragraph = document.querySelector('.js-paragraph')
+//Array
+let listNickname = [];
+//fetch
+fetch('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    });
