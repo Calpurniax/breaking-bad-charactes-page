98712740d2f91158d62ad33fa4d94dbb8cf9069d
Calpurniax <sofia.gracia@gmail.com>
2022-11-12 23:54:38 +0100

    fix favourite feature add localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 const btnSearch = document.querySelector('.js-btn');
 const inputSearch = document.querySelector('.js-input');
 const list = document.querySelector('.js-list');
-//const favList = document.querySelector('.js-favourite');
+const favList = document.querySelector('.js-favourite');
 //Array
 let allCharacters = [];
 let searchList = [];
@@ -13,17 +13,21 @@ fetch('https://breakingbadapi.com/api/characters')
   .then((response) => response.json())
   .then((data) => {
     allCharacters = data;
-    console.log(allCharacters);
-    renderAllCharacters(allCharacters);
+    renderAllCharacters(allCharacters, list);
+    if (localStorage.getItem('favChar')) {
+      favCharacters = JSON.parse(localStorage.getItem('favChar'));
+      renderAllCharacters(favCharacters, favList);
+    }
   });
 //bucle para pintar un array
-function renderAllCharacters(dataArray) {
+function renderAllCharacters(dataArray, htmlList) {
+  htmlList.innerHTML = '';
   for (const character of dataArray) {
-    renderCharacter(character);
+    renderCharacter(character, htmlList);
   }
 }
 //función para pintar cada personaje
-function renderCharacter(characterData) {
+function renderCharacter(characterData, htmlList) {
   const liElement = document.createElement('li');
   const articleElement = document.createElement('article');
   const imgElement = document.createElement('img');
@@ -40,11 +44,11 @@ function renderCharacter(characterData) {
   h3Element.appendChild(h3Text);
   h4Element.appendChild(statusText);
   articleElement.setAttribute('id', `${characterData.char_id}`);
-  articleElement.setAttribute('class', 'card');
-  //imgElement.setAttribute('class', 'card_img');
+  articleElement.setAttribute('class', 'section__list__article');
+  imgElement.setAttribute('class', 'section__list__article--img');
   //h3Element.setAttribute('class', 'card_title');
   //textElement.setAttribute('class', 'card_description');
-  list.appendChild(liElement);
+  htmlList.appendChild(liElement);
   addEvent();
 }
 //filtrar
@@ -55,8 +59,7 @@ function handleClick(event) {
     .then((response) => response.json())
     .then((data) => {
       searchList = data;
-      list.innerHTML = '';
-      renderAllCharacters(searchList);
+      renderAllCharacters(searchList, list);
     });
 }
 btnSearch.addEventListener('click', handleClick);
@@ -67,37 +70,28 @@ function handleFavourites(event) {
   const target = event.currentTarget;
   if (target.classList.contains('favourite')) {
     target.classList.remove('favourite');
-    const position = searchInArray(target);
+    const position = searchInArray(parseInt(target.id));
     favCharacters.splice(position, 1);
   } else {
     target.classList.add('favourite');
-    const idTarget = parseInt(target.id);
-    const selected = allCharacters.find(
-      (eachCharacter) => eachCharacter.char_id === idTarget
+    favCharacters.push(
+      allCharacters.find(
+        (eachCharacter) => eachCharacter.char_id === parseInt(target.id)
+      )
     );
-    console.log(selected);
-    favCharacters.push(selected);
   }
   console.log(favCharacters);
-  renderAllCharacters(favCharacters); //ahora mismo esta sumando cada personaje cada vez que sumas uno
+  localStorage.setItem('favChar', JSON.stringify(favCharacters));
+  renderAllCharacters(favCharacters, favList);
 }
 //añadir el evento de escucha a cada personaje
 function addEvent() {
-  const characters = document.querySelectorAll('.card');
+  const characters = document.querySelectorAll('.section__list__article');
   for (const eachCharacter of characters) {
     eachCharacter.addEventListener('click', handleFavourites);
   }
 }
 //buscar un elemento en el array
 function searchInArray(target) {
-  const index = favCharacters.indexOf(target);
-  return index;
+  return favCharacters.findIndex((each) => each.char_id === target);
 }
-//llamar a una tarjeta del array de datos
-/*function callData(idTarget) {
-  for (const character of allCharacters) {
-    if (character.char_id === idTarget) {
-      return character;
-    }
-  }
-}*/
