f5e3e5d50a8c0b38c4c088bdab39e61a951f8c60
Calpurniax <sofia.gracia@gmail.com>
2022-11-12 09:26:21 +0100

    add render and filter function

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,58 @@
-'use strict'
+'use strict';
 //query selector
 const btnSearch = document.querySelector('.js-btn');
-const inputSearch = document.querySelector('.js-text');
+const inputSearch = document.querySelector('.js-input');
 const list = document.querySelector('.js-list');
-const paragraph = document.querySelector('.js-paragraph')
+//const paragraph = document.querySelector('.js-favourite"');
 //Array
-let listNickname = [];
+let dataArray = [];
+let searchList = [];
 //fetch
 fetch('https://breakingbadapi.com/api/characters')
+  .then((response) => response.json())
+  .then((data) => {
+    dataArray = data;
+    console.log(dataArray);
+    renderAllCharacters(dataArray);
+  });
+//bucle para el array de datos
+function renderAllCharacters(dataArray) {
+  for (const character of dataArray) {
+    renderCharacter(character);
+  }
+}
+//función para pintar cada personaje
+function renderCharacter(characterData) {
+  const liElement = document.createElement('li');
+  const imgElement = document.createElement('img');
+  const h3Element = document.createElement('h3');
+  const h4Element = document.createElement('h4');
+  liElement.appendChild(imgElement);
+  liElement.appendChild(h3Element);
+  liElement.appendChild(h4Element);
+  imgElement.setAttribute('src', characterData.img);
+  imgElement.setAttribute('alt', `${characterData.name}`);
+  const h3Text = document.createTextNode(characterData.name);
+  const statusText = document.createTextNode(characterData.status);
+  h3Element.appendChild(h3Text);
+  h4Element.appendChild(statusText);
+  //liElement.setAttribute('class', 'card');
+  //imgElement.setAttribute('class', 'card_img');
+  //h3Element.setAttribute('class', 'card_title');
+  //h4Element.setAttribute('class', 'card_race');
+  //textElement.setAttribute('class', 'card_description');
+  list.appendChild(liElement);
+}
+//filtrar
+function handleClick(event) {
+  event.preventDefault();
+  const userSearch = inputSearch.value;
+  fetch(`https://breakingbadapi.com/api/characters?name=${userSearch}`)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
+      searchList = data;
+      list.innerHTML = '';
+      renderAllCharacters(searchList);
     });
+}
+btnSearch.addEventListener('click', handleClick);
