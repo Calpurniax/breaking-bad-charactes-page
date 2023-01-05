f541fb764436a2bbdd1e4c91d769754ee9d57b88
Calpurniax <sofia.gracia@gmail.com>
2023-01-05 11:42:15 +0100

    changing api to json

--- src/js/01-variables.js
+++ src/js/01-variables.js
@@ -4,7 +4,9 @@ const btnSearch = document.querySelector('.js-btn');
 const inputSearch = document.querySelector('.js-input');
 const list = document.querySelector('.js-list');
 const favList = document.querySelector('.js-favourite');
+const textParagraph = document.querySelector('.js-results-search');
 //Array
 let allCharacters = [];
 let searchList = [];
 let favCharacters = [];
+let filterCharacters = [];
